8c23cbe890bf2e4ee11a6e2365afdecd41d5fa6d
Hanzhi Yin <yinhanzhi@gmail.com>
2020-05-10 17:34:01 -0500

    GUI initialised

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -1,30 +1,66 @@
-/*
-  ==============================================================================
-
-    This file was auto-generated!
-
-  ==============================================================================
-*/
-
 #include "MainComponent.h"
 
-//==============================================================================
-MainComponent::MainComponent() {
-    // Make sure you set the size of the component after
-    // you add any child components.
-    this->setSize (800,600);
+//// ==============================================================================
+//// Constructors and destructors
+//// ==============================================================================
 
+MainComponent::MainComponent() :
+    midiKeyboardComponent(midiKeyboardState, juce::MidiKeyboardComponent::horizontalKeyboard),
+    audioVisualiserComponent(2),
+    audioSource(midiKeyboardState)
+{
+    // Initialise audioSettings button
+    audioSettings.addListener(this);
+    addAndMakeVisible(audioSettings);
+    // Initialise midiKeyboardComponent
+    midiKeyboardComponent.setOctaveForMiddleC(4);
+    addAndMakeVisible(midiKeyboardComponent);
+    // Initialise tailOn Label
+    addAndMakeVisible(tailOnLabel);
+    // Initialise tailOnSlider
+    tailOnSlider.addListener(this);
+    tailOnSlider.setSliderStyle(Slider::SliderStyle::LinearBar);
+    tailOnSlider.setRange(0.05, 0.95);
+    tailOnSlider.setValue(0.95);
+    addAndMakeVisible(tailOnSlider);
+    // Initialise tailOff Label
+    addAndMakeVisible(tailOffLabel);
+    // Initialise tailOffSlider
+    tailOffSlider.addListener(this);
+    tailOffSlider.setSliderStyle(Slider::SliderStyle::LinearBar);
+    tailOffSlider.setRange(0.05, 0.95);
+    tailOffSlider.setValue(0.05);
+    addAndMakeVisible(tailOffSlider);
+    // Initialise synthesiserList
+    addAndMakeVisible(synthesiserList);
+    // Initialise synthesiserVoiceAdder
+    synthesiserVoiceAdder.addListener(this);
+    synthesiserVoiceAdder.addItemList(
+            {"Sine", "Square", "Sawtooth", "Triangle", "Custom Sound Font"}, 1);
+    synthesiserVoiceAdder.setText("Select the voice to add", dontSendNotification);
+    addAndMakeVisible(synthesiserVoiceAdder);
+    // Initialise audioVisualiserComponent
+    addAndMakeVisible(audioVisualiserComponent);
+    // Initialise CPU Usage Label
+    addAndMakeVisible(cpuUsageLabel);
+    // Initialise CPU Usage
+    addAndMakeVisible(cpuUsage);
+    // Make sure we set the size of the component at last!
+    this->setSize (852,608);
     // Some platforms require permissions to open input channels so request that here
     if (RuntimePermissions::isRequired (RuntimePermissions::recordAudio) &&
         !RuntimePermissions::isGranted (RuntimePermissions::recordAudio)) {
         RuntimePermissions::request (RuntimePermissions::recordAudio,
-                [&] (bool granted) {
-                if (granted) { this->setAudioChannels(2, 2); }
-            });
+                [&] (bool granted) { if (granted) {
+                    this->setAudioChannels(2, 2);
+                }
+        });
     } else {
-        // Specify the number of input and output channels that we want to open
-        this->setAudioChannels (2, 2);
+        this->setAudioChannels(2, 2);
     }
+    // Timer setup
+    this->startTimer(500);
+    // TODO value tree listener initialisation
 }
 
 MainComponent::~MainComponent() {
@@ -32,44 +68,69 @@ MainComponent::~MainComponent() {
     this->shutdownAudio();
 }
 
-//==============================================================================
+//// ==============================================================================
+//// Audio related function
+//// ==============================================================================
+
 void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRate) {
-    // This function will be called when the audio device is started, or when
-    // its settings (i.e. sample rate, block size, etc) are changed.
 
-    // You can use this function to initialise any resources you might need,
-    // but be careful - it will be called on the audio thread, not the GUI thread.
-
-    // For more details, see the help for AudioProcessor::prepareToPlay()
 }
 
 void MainComponent::getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill) {
-    // Your audio-processing code goes here!
 
-    // For more details, see the help for AudioProcessor::getNextAudioBlock()
-
-    // Right now we are not producing any data, in which case we need to clear the buffer
-    // (to prevent the output of random noise)
     bufferToFill.clearActiveBufferRegion();
 }
 
 void MainComponent::releaseResources() {
-    // This will be called when the audio device stops, or when it is being
-    // restarted due to a setting change.
 
-    // For more details, see the help for AudioProcessor::releaseResources()
 }
 
-//==============================================================================
+//// ==============================================================================
+//// Graphical interface rendering
+//// ==============================================================================
+
 void MainComponent::paint (Graphics& g) {
-    g.fillAll (this->getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+    g.fillAll(this->getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
 }
 
 void MainComponent::resized() {
-    // This is called when the MainContentComponent is resized.
-    // If you add any child components, this is where you should
-    // update their positions.
+    Rectangle<int> globalBound = this->getLocalBounds();
+    globalBound.removeFromRight(8);
+    globalBound.removeFromBottom(8);
+    globalBound.removeFromLeft(8);
+    globalBound.removeFromTop(8);
+
+    Rectangle<int> leftColumn = globalBound.removeFromLeft(608);
+    Rectangle<int> rightColumn = globalBound.removeFromRight(236);
+
+    this->audioSettings.setBounds(
+            leftColumn.removeFromTop(24).removeFromLeft(120));
+    leftColumn.removeFromTop(8);
+    this->midiKeyboardComponent.setBounds(leftColumn.removeFromTop(64));
+    leftColumn.removeFromTop(8);
+    this->audioVisualiserComponent.setBounds(leftColumn.removeFromTop(456));
+    leftColumn.removeFromTop(8);
+    this->cpuUsageLabel.setBounds(leftColumn.removeFromLeft(96));
+    this->cpuUsage.setBounds(leftColumn.removeFromLeft(72));
+
+    rightColumn.removeFromTop(36);
+    Rectangle<int> onSlider = rightColumn.removeFromTop(24);
+    this->tailOnLabel.setBounds(onSlider.removeFromLeft(60));
+    this->tailOnSlider.setBounds(onSlider);
+    rightColumn.removeFromTop(8);
+    Rectangle<int> offSlider = rightColumn.removeFromTop(24);
+    this->tailOffLabel.setBounds(offSlider.removeFromLeft(60));
+    this->tailOffSlider.setBounds(offSlider);
+    rightColumn.removeFromTop(12);
+
+    this->synthesiserList.setBounds(rightColumn.removeFromTop(456));
+    rightColumn.removeFromTop(8);
+    this->synthesiserVoiceAdder.setBounds(rightColumn);
 }
+
+//// ==============================================================================
+//// Component callback functions.
+//// ==============================================================================
 
 void MainComponent::buttonClicked(Button *button) {
 
@@ -77,4 +138,40 @@ void MainComponent::buttonClicked(Button *button) {
 
 void MainComponent::sliderValueChanged(Slider *slider) {
 
+}
+
+void MainComponent::comboBoxChanged(ComboBox *comboBoxThatHasChanged) {
+
+}
+
+//// ==============================================================================
+//// Value tree listener
+//// ==============================================================================
+
+void MainComponent::valueTreePropertyChanged(ValueTree &treeWhosePropertyHasChanged, const Identifier &property) {
+
+}
+
+//// ==============================================================================
+//// Timer callback
+//// ==============================================================================
+
+void MainComponent::timerCallback() {
+
+}
+
+//// ==============================================================================
+//// Private functions
+//// ==============================================================================
+
+void MainComponent::openAudioSettings() {
+    auto audioSettingsPanel = std::make_unique<juce::AudioDeviceSelectorComponent>(this->deviceManager, 0, 2, 0, 2, true, true, true, true);
+    audioSettingsPanel->setSize(500, 270);
+    juce::DialogWindow::LaunchOptions dialogWindow;
+    dialogWindow.useNativeTitleBar = true;
+    dialogWindow.resizable = false;
+    dialogWindow.dialogTitle = "Audio Settings";
+    dialogWindow.dialogBackgroundColour = this->getLookAndFeel().findColour(ResizableWindow::backgroundColourId);
+    dialogWindow.content.setOwned(audioSettingsPanel.release());
+    dialogWindow.launchAsync();
 }
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -13,47 +13,139 @@
 #include "SynthesiserSource.h"
 #include "SynthesiserData.h"
 
-//==============================================================================
-/*
-    This component lives inside our window, and this is where you should put all
-    your controls and content.
-*/
+/**
+ * This component lives inside our window, and this is where we put all our controls and content.
+ * This component manages all the graphical components in our application,
+ * listens to all the components action,
+ * and also manages the audio activity in our application.
+ * It has the ability to update the application-wide values that is shared among all the classes,
+ * and has the ability to response to the application-wide value change.
+ * It also has a internal timer to update the CPU usage of the current audio device.
+ */
 class MainComponent :
         public AudioAppComponent,
         public Button::Listener,
         public Slider::Listener,
-        public juce::ValueTree::Listener
+        public ComboBox::Listener,
+        public juce::ValueTree::Listener,
+        public juce::Timer
 {
 public:
-    //==============================================================================
+//// ==============================================================================
+//// Constructors and destructors
+//// ==============================================================================
+
+    /** Constructor of the main component */
     MainComponent();
+
+    /** Destructor of the main component */
     ~MainComponent() override;
 
-    //==============================================================================
+//// ==============================================================================
+//// Audio related function
+//// ==============================================================================
+
+    /**
+     * This function will be called when the audio device is started,
+     * or when its settings (i.e. sample rate, block size, etc) are changed.
+     * @see AudioProcessor::prepareToPlay()
+     * @param samplesPerBlockExpected Samples contained in buffers which is passed into the getNextAudioBlock().
+     * @param sampleRate the expected sample rate of our synthesiser
+     */
     void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override;
+
+    /**
+     * Render the next audio buffer.
+     * This function accepts a reference to the buffer we need to fill.
+     * @param bufferToFill the buffer to fill in with expected audio data
+     */
     void getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill) override;
+
+    /**
+     * Called when the audio source is changing from prepared state to unprepared state.
+     * This function is essentially NOP, though we still have to call the synthesiser's releaseResource() formally.
+     * @see SynthesiserAudioSource::releaseResource()
+     */
     void releaseResources() override;
 
-    //==============================================================================
+//// ==============================================================================
+//// Graphical interface rendering
+//// ==============================================================================
+
+    /**
+     * Called when the main component is initialised
+     * @param g the graphics of the main component. User unreachable. Managed by JUCE.
+     */
     void paint (Graphics& g) override;
+
+    /**
+     * Called when the size of the main component is initialised or resized
+     * This is the place where the size of each sub components are set up.
+     */
     void resized() override;
 
-    //==============================================================================
+//// ==============================================================================
+//// Component callback functions.
+//// ==============================================================================
+    /**
+     * Called when a button is clicked
+     * @param button the button which is clicked
+     */
     void buttonClicked(Button *button) override;
+
+    /**
+     * Called when a slider has been modified
+     * @param slider the slider which has been modified
+     */
     void sliderValueChanged(Slider *slider) override;
 
-private:
+    /**
+     * Called when a combo box has its menu changed
+     * @param comboBoxThatHasChanged the combo box which is clicked
+     */
+    void comboBoxChanged(ComboBox *comboBoxThatHasChanged) override;
 
-    TextButton openAudioSettings {"openAudioSettings"};
+//// ==============================================================================
+//// Value tree listener
+//// ==============================================================================
+    /**
+     * Called when the value tree we listen to has a property change
+     * @param treeWhosePropertyHasChanged the value tree that has the property changed
+     * @param property the property which is changed
+     */
+    void valueTreePropertyChanged (ValueTree &treeWhosePropertyHasChanged, const Identifier &property) override;
+
+//// ==============================================================================
+//// Timer callback
+//// ==============================================================================
+    /**
+     * Called periodically to carry out certain task
+     */
+    void timerCallback() override;
+
+private:
+    TextButton audioSettings {"Audio Settings"};
+    // TODO should have an selector to choose input devices?
+
     MidiKeyboardComponent midiKeyboardComponent;
     MidiKeyboardState midiKeyboardState;
+
+    Label tailOnLabel {"tailOn", "Tail on:"};
+    Slider tailOnSlider {"tailOnSlider"};
+    Label tailOffLabel {"tailOff", "Tail off:"};
+    Slider tailOffSlider {"tailOffSlider"};
+
     SynthesiserList synthesiserList;
-    Slider tailOnSlider;
-    Slider tailOffSlider;
+    ComboBox synthesiserVoiceAdder;
+
+    AudioVisualiserComponent audioVisualiserComponent;
+    Label cpuUsageLabel {"cpuUsageLabel", "CPU usage:"};
+    Label cpuUsage {"cpuUsage", "0.00 %"};
 
     SynthesiserAudioSource audioSource;
-
     SynthesiserData synthesiserData;
+
+    inline void openAudioSettings();
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
