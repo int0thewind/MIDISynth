5cb948a6f911c835e75292532232662241596fc8
Hanzhi Yin <yinhanzhi@gmail.com>
2020-05-10 14:12:14 -0500

    Implement SynthesiserAudioSource

--- Source/SynthesiserSource.cpp
+++ Source/SynthesiserSource.cpp
@@ -10,10 +10,52 @@
 
 #include "SynthesiserSource.h"
 
+//// ==============================================================================
+//// SynthesiserAudioSound Class
+//// ==============================================================================
+
 bool SynthesiserAudioSound::appliesToNote(int midiNoteNumber) {
     return true;
 }
 
 bool SynthesiserAudioSound::appliesToChannel(int midiChannel) {
     return true;
+}
+
+//// ==============================================================================
+//// SynthesiserAudioSource Class
+//// ==============================================================================
+
+SynthesiserAudioSource::SynthesiserAudioSource(MidiKeyboardState &state)
+    : midiKeyboardState(state) {
+    this->synthesiser.addSound(new SynthesiserAudioSound());
+}
+
+void SynthesiserAudioSource::prepareToPlay(int samplesPerBlockExpected, double sampleRate) {
+    this->synthesiser.setCurrentPlaybackSampleRate(sampleRate);
+}
+
+void SynthesiserAudioSource::releaseResources() {
+    // Do nothing!
+}
+
+void SynthesiserAudioSource::getNextAudioBlock(const AudioSourceChannelInfo &bufferToFill) {
+    bufferToFill.clearActiveBufferRegion();
+    MidiBuffer incomingMidi;
+    this->midiKeyboardState.processNextMidiBuffer (incomingMidi, bufferToFill.startSample,
+                                               bufferToFill.numSamples, true);
+    this->synthesiser.renderNextBlock (*bufferToFill.buffer, incomingMidi,
+                                 bufferToFill.startSample, bufferToFill.numSamples);
+}
+
+void SynthesiserAudioSource::removeVoice(int index) {
+    this->synthesiser.removeVoice(index);
+}
+
+void SynthesiserAudioSource::addVoice(SynthesiserVoice *voice) {
+    this->synthesiser.addVoice(voice);
+}
+
+void SynthesiserAudioSource::removeAllVoices() {
+    this->synthesiser.clearVoices();
 }
--- Source/SynthesiserSource.h
+++ Source/SynthesiserSource.h
@@ -110,19 +110,61 @@ public:
 
 class SynthesiserAudioSource : public AudioSource {
 public:
-    explicit SynthesiserAudioSource(const MidiKeyboardState& state);
+    /**
+     * This constructor receives the alias of a midi keyboard state from the main component
+     * Also, it sets the synthesiser's voice to our synthesiserAudioSound, which accepts all channels and notes
+     * @param state the alias of the midiKeyboardState from the main component.
+     */
+    explicit SynthesiserAudioSource(MidiKeyboardState& state);
 
+    /**
+     * Called when the audio source is changing from unprepared state to prepared state
+     * We set the internal synthesiser's sample rate to the expected sample rate.
+     * @param samplesPerBlockExpected Samples contained in buffers which is passed into the getNextAudioBlock().
+     * @param sampleRate the expected sample rate of our synthesiser
+     */
     void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override;
 
+    /**
+     * Called when the audio source is changing from prepared state to unprepared state.
+     * This function is NOP
+     */
     void releaseResources() override;
 
+    /**
+     * Called when the next sound is rendered.
+     * This function accepts a reference to the buffer we need to fill.
+     * @param bufferToFill the buffer to fill in with expected audio data
+     */
     void getNextAudioBlock(const AudioSourceChannelInfo& bufferToFill) override;
 
-    void addVoice();
+    /**
+     * Add voice to the internal synthesiser
+     * @param voice
+     */
+    void addVoice(SynthesiserVoice* voice);
 
-    void removeVoice();
+    /**
+     * Remove voice by its index
+     * @todo would the rest of the index automatically update?
+     * @param index
+     */
+    void removeVoice(int index);
+
+    /**
+     * Remove all the voices in the internal synthesiser.
+     */
+    void removeAllVoices();
 
 private:
+    /**
+     * Reference of the MIDI Keyboard State
+     * The whole application should only accepts one MIDI Keyboard State, which is from the main component
+     */
     MidiKeyboardState& midiKeyboardState;
+
+    /**
+     * The internal synthesiser that combines different voices.
+     */
     Synthesiser synthesiser;
 };
